3afad17eb01a3442adaeaba907344cfa3ad08e84
fritzgt <tuanis@live.com>
2019-08-26 20:29:32 +0300

    creating branch

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 
+// installed this library
+const jwt = require('jsonwebtoken');
+
 const Users = require('../users/users-model.js');
 
 // for endpoints beginning with /api/auth
@@ -26,7 +29,7 @@ router.post('/login', (req, res) => {
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
+          message: `Welcome ${user.username}!`
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
